ff34f4ae56d7923c41e772d519c40c13c2420f40
Lars Ullrich <lullrich.py@gmail.com>
2016-06-15 01:54:16 +0200

    fixed a line break in the middle of a word...

--- simple_feedfinder.R
+++ simple_feedfinder.R
@@ -1,26 +1,28 @@
 library("rvest")
 library("lubridate")
 
-urls <- c('http://wsj.com',
-          'http://www.allure.com',
-          'http://www.theinsider.com',
-          'http://cnet.com',
-          'http://venturebeat.com',
-          'http://www.topspeed.com',
-          'http://thedailyworld.com',
-          'http://games.com',
-          'http://www.religionnews.com',
-          'http://blogs.berkeley.edu',
-          'http://www.sbnation.com',
-          'http://www.polygon.com',
-          'http://nytimes.com',
-          'http://www.thefrisky.com',
-          'http://telegram.com',
-          'http://yahoo.com',
-          'http://www.nbcnews.com',
-          'http://thedailypage.com',
-          'http://www.popsci.com',
-          'http://www.pbs.org')
+urls <- c(
+  'http://wsj.com',
+  'http://www.allure.com',
+  'http://www.theinsider.com',
+  'http://cnet.com',
+  'http://venturebeat.com',
+  'http://www.topspeed.com',
+  'http://thedailyworld.com',
+  'http://games.com',
+  'http://www.religionnews.com',
+  'http://blogs.berkeley.edu',
+  'http://www.sbnation.com',
+  'http://www.polygon.com',
+  'http://nytimes.com',
+  'http://www.thefrisky.com',
+  'http://telegram.com',
+  'http://yahoo.com',
+  'http://www.nbcnews.com',
+  'http://thedailypage.com',
+  'http://www.popsci.com',
+  'http://www.pbs.org'
+)
 
 ###########################################
 
@@ -32,26 +34,35 @@ get_links <- function(base_url) {
   if (is.list(res)) {
     message("response ok, looking for feeds")
     
-    res <- html_nodes(res, xpath = "//link[contains(@type, 'rss') or c
-                      ontains(@type, 'atom') or contains(@type, 'rdf')]") %>% 
-      sapply(html_attr, "href") %>% 
-      Filter(is.character, .) %>% 
-      sapply(url_absolute, base_url) %>% 
+    res <-
+      html_nodes(res,
+                 xpath = "//link[contains(@type, 'rss') or
+                 contains(@type, 'atom') or contains(@type, 'rdf')]") %>%
+      sapply(html_attr, "href") %>%
+      Filter(is.character, .) %>%
+      sapply(url_absolute, base_url) %>%
       unique()
-    
-    return(res)
+    if (length(res) > 0) {
+      print(res)
+      return(res)
+    }
+    else {
+      return(NA)
+    }
   }
   else {
     message("skipping")
-    warning(paste("skipped", base_url, "- not responding in time."), call. = FALSE)
+    warning(paste("skipped", base_url, "- not responding in time."),
+            call. = FALSE)
     
-    return(NULL)
+    return(NA)
   }
 }
 
 st <- now()
-feeds <- lapply(urls, get_links) %>% Filter(is.character, .) %>% unlist()  
+feeds <-
+  lapply(urls, get_links) %>% Filter(is.character, .) %>% unlist()
 et <- now()
-et-st
+et - st
 message(paste("Found", length(feeds), "feeds in", length(urls), "URLs."))
 feeds
